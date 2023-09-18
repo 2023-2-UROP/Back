0969521e707834c81ac8db4016faf4b17ff58f41
김지수 <zsu@jisu-MacBookAir-2.local>
2023-09-18 20:57:36 +0900

    all

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views as main_views
 
 urlpatterns = [
-    path('', main_views.index, name="index"),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]

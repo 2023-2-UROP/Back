ecaea43335eae328e347a93feebea24de4571fc6
김지수 <zsu@jisu-MacBookAir-2.local>
2023-09-23 22:58:07 +0900

    login and sign up

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import json, re, traceback
+import json, re, traceback, bcrypt, jwt, my_settings
 from django.http import JsonResponse
 from django.views import View
 from django.core.exceptions import ValidationError
@@ -54,5 +54,21 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
 
-# Create your views here.
+        try:
+            email = data.get('email')
+            phone = data.get('phone')
+            password = data.get('password')
+
+            if Account.objects.filter(Q(email=email) | Q(phone=phone)).exists():
+                account = Account.objects.get(Q(email=email) | Q(phone=phone))
+                if bcrypt.checkpw(password.encode('utf-8'), account.password.encode('utf-8')):
+                    token = jwt.encode({'email' : email}, my_settings.SECRET['secret'], algorithm='HS256')
+                    return JsonResponse({'message' : 'SUCCESS'}, status=200)
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=401)
+            return JsonResponse({"message": "INVALID_USER"}, status=401)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)

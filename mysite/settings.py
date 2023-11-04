41532f859a3d62d25d40b26e0ff0463bf583708f
김지수 <zsu@jisu-MacBookAir-2.local>
2023-11-04 18:50:41 +0900

    A

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = my_settings.SECRET['secret']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'
+ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com",
     # "127.0.0.1"
 ]

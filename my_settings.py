b1f32c61840217e100b6492d3778aea141b0b6b1
김지수 <zsu@jisu-MacBookAir-2.local>
2023-11-04 18:45:33 +0900

    F

---
+++ my_settings.py
@@ -0,0 +1,18 @@
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dbusers',
+#         'USER': 'root',
+#         'PASSWORD': 'wltn',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+SECRET = {
+         'secret':'django-insecure-u8k6cu%7@nsl9i*a%7$a%q2&ot8i_yb_2rp*vxxz&j0u5nw28w'
+}
+

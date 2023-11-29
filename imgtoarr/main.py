9e1625c094795efcff33ad0861b0e2bbb0ad300d
김지수 <zsu@jisu-MacBookAir-2.local>
2023-11-29 18:42:15 +0900

    modify code

--- imgtoarr/main.py
+++ imgtoarr/main.py
@@ -37,11 +37,51 @@
 #     narr = "numbers= {}".format(numbers)
 #     print(narr)
 
+# import os
+# from fuction222 import *
+# import cv2
+# import numpy as np
+# image_path = 'imgtoarr/sudoku_capture.png'
+# def img_make_arr(image_path):
+#     # Set up
+#     cv2.setUseOptimized(True)
+#     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+#
+#     # Read image
+#     img = cv2.imread(image_path)
+#     height_img = 720
+#     width_img = 720
+#     model = intializePredectionModel()
+#
+#     img = cv2.resize(img, (width_img, height_img))
+#     img_threshold = preProcess(img)
+#     contours, hierarchy = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#
+#     img_big_contour = img.copy()
+#
+#     biggest, max_area = biggestContour(contours)
+#     if biggest.size != 0:
+#         biggest = reorder(biggest)
+#         cv2.drawContours(img_big_contour, biggest, -1, (0, 0, 255), 20)
+#         pts1 = np.float32(biggest)
+#         pts2 = np.float32([[0, 0], [width_img, 0], [0, height_img], [width_img, height_img]])
+#         matrix = cv2.getPerspectiveTransform(pts1, pts2)
+#         img_warp_binary = cv2.warpPerspective(img_threshold, matrix, (width_img, height_img))
+#
+#         boxes = splitBoxes(img_warp_binary)
+#
+#         numbers = getPredection(boxes, model)
+#
+#         return numbers
+
+# result_numbers = img_make_arr(image_path)
+
 import os
 from fuction222 import *
 import cv2
 import numpy as np
 image_path = 'imgtoarr/sudoku_capture.png'
+# pathImage = '/Users/zsu/PycharmProjects/pythonProject2/sudoku_capture.png'
 def img_make_arr(image_path):
     # Set up
     cv2.setUseOptimized(True)
@@ -74,4 +114,4 @@ def img_make_arr(image_path):
 
         return numbers
 
-# result_numbers = img_make_arr(image_path)
+print(img_make_arr(pathImage))
